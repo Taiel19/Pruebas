8be8705f548b8243fd12d68408f4b32e886d2bac
Taiel <taielvinograd@gmail.com>
2021-07-03 03:04:22 -0300

    error

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use( express.json({ extended: true }));
 
 // puerto de la app
-const Port = process.env.Port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(Port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${Port}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
